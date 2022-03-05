3c26fa04f4434b6368f2e3a764a58d2c762bf112
Newton Hoang <newtonhoang99@gmail.com>
2022-03-05 09:38:10 -0800

    last styling fix

--- client/src/components/SkillForm/index.js
+++ client/src/components/SkillForm/index.js
@@ -40,7 +40,7 @@ const PostForm = ({ userId, handleFormSubmit, formData, setFormData }) => {
           </div>
 
           <div className="col-12 col-lg-3">
-            <button className="btn btn-info btn-block py-3" type="submit">
+            <button className="btn btn-info btn-block" type="submit">
               Post Review
             </button>
           </div>
